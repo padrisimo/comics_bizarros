80d1051d010eb837218ae8e18fb4f2a2a8444f7c
Padrisimo vs hipstercoders <chamizho@gmail.com>
2016-03-25 22:53:52 +0100

    ya era hora

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 // Note: See http://blog.garstasio.com/you-dont-need-jquery/ and http://youmightnotneedjquery.com/ for JS commands that don't require jQuery
 $(document).ready(function(){
+
 });
+
+function validar() {
+
+     alert("Enviado!");
+
+
+}

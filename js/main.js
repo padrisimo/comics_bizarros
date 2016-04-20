6bb9125d3da5a2d08620b3f002806cc3de27a64f
Padrisimo vs hipstercoders <chamizho@gmail.com>
2016-04-20 13:55:17 +0200

    ajax pino lo deja fino

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-// Note: See http://blog.garstasio.com/you-dont-need-jquery/ and http://youmightnotneedjquery.com/ for JS commands that don't require jQuery
-$(document).ready(function(){
+// ou yea
+$(function() {
+    $('#mandar').click(function() {
+     /* Rock n Roll */
+            var data = {
+                name: $("#nombre").val(),
+                email: $("#email").val(),
+                message: $("#ta").val()
+            };
+            $.ajax({
+                type: "POST",
+                url: "sendbymail.php",
+                data: data,
+                success: function(){
+                    // hara que un "enviado con exito" en hide con .succes aparezca
+                    $('.success').fadeIn(1000);
+                    // limpio los inputs tras el envio, ya tu sabeh.
+                    $('#nombre').val('');
+                    $('#email').val('');
+                    $('#ta').val('');
+                    $('#ta').focus();
+                    // oculto el mensaje pasado 5sec
+                    setTimeout("$('.success').fadeOut(3000);", 5000);
+                }
+            });
 
+        return false;
+    });
 });
-
-function validar() {
-
-     alert("Enviado!");
-
-
-}
+// mi codigo comentado hace llorar al niño Jesús pero me ayuda con mis milongas
--- sendbymail.php
+++ sendbymail.php
@@ -1,37 +1,10 @@
 <?php
+if($_POST){
+    $name = $_POST['nombre'];
+    $email = $_POST['email'];
+    $message = $_POST['ta'];
 
-
-if(isset($_POST['email'])) {
-
-// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "comicsbizarros@gmail.com";
-$email_subject = "Contacto desde el sitio web";
-
-// Aquí se deberían validar los datos ingresados por el usuario
-// if(
-// !isset($_POST['nombre']) ||
-// !isset($_POST['email']) ||
-// !isset($_POST['texto'])) {
-
-// echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-// echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-// die();
-// }
-
-$email_message = "Detalles del formulario de contacto:\n\n";
-$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
-$email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Comentarios: " . $_POST['texto'] . "\n\n";
-
-
-// Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
-
-Header("Location: index.html");
-
-echo "¡El formulario se ha enviado con éxito!";
+//y asi fue como miles de cachondas admiradoras me mandaron correos.
+    mail("comicsbizarros@gmail.com", $name, $email, $message);
 }
 ?>
